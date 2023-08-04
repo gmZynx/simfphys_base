736935ba89a01b3cbb656b7deb8a3d38d5d008e0
Redox <mail@redox.to>
2023-08-04 05:35:31 +0100

    Style

--- lua/entities/gmod_sent_vehicle_fphysics_base/damage.lua
+++ lua/entities/gmod_sent_vehicle_fphysics_base/damage.lua
@@ -1,9 +1,9 @@
 function ENT:ApplyDamage( damage, type )
-	if type == DMG_BLAST then 
+	if type == DMG_BLAST then
 		damage = damage * 10
 	end
 
-	if type == DMG_BULLET then 
+	if type == DMG_BULLET then
 		damage = damage * 2
 	end
 
@@ -68,7 +68,7 @@ function ENT:HurtPlayers( damage )
 
 	for i = 1, table.Count( self.PassengerSeats ) do
 		local Passenger = self.pSeat[i]:GetDriver()
-		
+
 		if not IsValid(Passenger) then continue end
 
 		local dmginfo = DamageInfo()
@@ -84,7 +84,7 @@ end
 function ENT:ExplodeVehicle()
 	if not IsValid( self ) then return end
 	if self.destroyed then return end
-	
+
 	self.destroyed = true
 
 	local ply = self.EntityOwner
@@ -93,7 +93,7 @@ function ENT:ExplodeVehicle()
 	Col.r = Col.r * 0.8
 	Col.g = Col.g * 0.8
 	Col.b = Col.b * 0.8
-	
+
 	if self.GibModels then
 		local bprop = ents.Create( "gmod_sent_vehicle_fphysics_gib" )
 		bprop:SetModel( self.GibModels[1] )
@@ -102,16 +102,16 @@ function ENT:ExplodeVehicle()
 		bprop.MakeSound = true
 		bprop:Spawn()
 		bprop:Activate()
-		bprop:GetPhysicsObject():SetVelocity( self:GetVelocity() + Vector(math.random(-5,5),math.random(-5,5),math.random(150,250)) ) 
+		bprop:GetPhysicsObject():SetVelocity( self:GetVelocity() + Vector(math.random(-5,5),math.random(-5,5),math.random(150,250)) )
 		bprop:GetPhysicsObject():SetMass( self.Mass * 0.75 )
 		bprop.DoNotDuplicate = true
 		bprop:SetColor( Col )
 		bprop:SetSkin( skin )
-		
+
 		self.Gib = bprop
-		
+
 		simfphys.SetOwner( ply , bprop )
-		
+
 		if IsValid( ply ) then
 			undo.Create( "Gib" )
 			undo.SetPlayer( ply )
@@ -120,11 +120,11 @@ function ENT:ExplodeVehicle()
 			undo.Finish( "Gib" )
 			ply:AddCleanup( "Gibs", bprop )
 		end
-		
+
 		bprop.Gibs = {}
 		for i = 2, table.Count( self.GibModels ) do
 			local prop = ents.Create( "gmod_sent_vehicle_fphysics_gib" )
-			prop:SetModel( self.GibModels[i] )			
+			prop:SetModel( self.GibModels[i] )
 			prop:SetPos( self:GetPos() )
 			prop:SetAngles( self:GetAngles() )
 			prop:SetOwner( bprop )
@@ -133,26 +133,26 @@ function ENT:ExplodeVehicle()
 			prop.DoNotDuplicate = true
 			bprop:DeleteOnRemove( prop )
 			bprop.Gibs[i-1] = prop
-			
+
 			local PhysObj = prop:GetPhysicsObject()
 			if IsValid( PhysObj ) then
 				PhysObj:SetVelocityInstantaneous( VectorRand() * 500 + self:GetVelocity() + Vector(0,0,math.random(150,250)) )
 				PhysObj:AddAngleVelocity( VectorRand() )
 			end
-			
-			
+
+
 			simfphys.SetOwner( ply , prop )
 		end
 	else
-		
+
 		local bprop = ents.Create( "gmod_sent_vehicle_fphysics_gib" )
-		bprop:SetModel( self:GetModel() )			
+		bprop:SetModel( self:GetModel() )
 		bprop:SetPos( self:GetPos() )
 		bprop:SetAngles( self:GetAngles() )
 		bprop.MakeSound = true
 		bprop:Spawn()
 		bprop:Activate()
-		bprop:GetPhysicsObject():SetVelocity( self:GetVelocity() + Vector(math.random(-5,5),math.random(-5,5),math.random(150,250)) ) 
+		bprop:GetPhysicsObject():SetVelocity( self:GetVelocity() + Vector(math.random(-5,5),math.random(-5,5),math.random(150,250)) )
 		bprop:GetPhysicsObject():SetMass( self.Mass * 0.75 )
 		bprop.DoNotDuplicate = true
 		bprop:SetColor( Col )
@@ -160,11 +160,11 @@ function ENT:ExplodeVehicle()
 		for i = 0, self:GetNumBodyGroups() do
 			bprop:SetBodygroup(i, self:GetBodygroup(i))
 		end
-		
+
 		self.Gib = bprop
-		
-		simfphys.SetOwner( ply , bprop )
-		
+
+		simfphys.SetOwner( ply, bprop )
+
 		if IsValid( ply ) then
 			undo.Create( "Gib" )
 			undo.SetPlayer( ply )
@@ -173,14 +173,14 @@ function ENT:ExplodeVehicle()
 			undo.Finish( "Gib" )
 			ply:AddCleanup( "Gibs", bprop )
 		end
-		
+
 		if self.CustomWheels == true and not self.NoWheelGibs then
 			bprop.Wheels = {}
 			for i = 1, table.Count( self.GhostWheels ) do
 				local Wheel = self.GhostWheels[i]
-				if IsValid(Wheel) then
+				if IsValid( Wheel ) then
 					local prop = ents.Create( "gmod_sent_vehicle_fphysics_gib" )
-					prop:SetModel( Wheel:GetModel() )			
+					prop:SetModel( Wheel:GetModel() )
 					prop:SetPos( Wheel:LocalToWorld( Vector(0,0,0) ) )
 					prop:SetAngles( Wheel:LocalToWorldAngles( Angle(0,0,0) ) )
 					prop:SetOwner( bprop )
@@ -191,7 +191,7 @@ function ENT:ExplodeVehicle()
 					prop.DoNotDuplicate = true
 					bprop:DeleteOnRemove( prop )
 					bprop.Wheels[i] = prop
-					
+
 					simfphys.SetOwner( ply , prop )
 				end
 			end
@@ -199,12 +199,10 @@ function ENT:ExplodeVehicle()
 	end
 
 	local Driver = self:GetDriver()
-	if IsValid( Driver ) then
-		if self.RemoteDriver ~= Driver then
-			Driver:TakeDamage( Driver:Health() + Driver:Armor(), self.LastAttacker or Entity(0), self.LastInflictor or Entity(0) )
-		end
+	if IsValid( Driver ) and self.RemoteDriver ~= Driver then
+		Driver:TakeDamage( Driver:Health() + Driver:Armor(), self.LastAttacker or Entity(0), self.LastInflictor or Entity(0) )
 	end
-	
+
 	if self.PassengerSeats then
 		for i = 1, table.Count( self.PassengerSeats ) do
 			local Passenger = self.pSeat[i]:GetDriver()
@@ -214,35 +212,35 @@ function ENT:ExplodeVehicle()
 		end
 	end
 
-	self:Extinguish() 
-	
+	self:Extinguish()
+
 	self:OnDestroyed()
-	
+
 	hook.Run( "simfphysOnDestroyed", self, self.Gib )
-	
+
 	self:Remove()
 end
 
 function ENT:OnTakeDamage( dmginfo )
 	if not self:IsInitialized() then return end
-	
+
 	if hook.Run( "simfphysOnTakeDamage", self, dmginfo ) then return end
-	
-	local Damage = dmginfo:GetDamage() 
-	local DamagePos = dmginfo:GetDamagePosition() 
+
+	local Damage = dmginfo:GetDamage()
+	local DamagePos = dmginfo:GetDamagePosition()
 	local Type = dmginfo:GetDamageType()
 	local Driver = self:GetDriver()
-	
-	self.LastAttacker = dmginfo:GetAttacker() 
+
+	self.LastAttacker = dmginfo:GetAttacker()
 	self.LastInflictor = dmginfo:GetInflictor()
-	
+
 	if simfphys.DamageEnabled then
 		net.Start( "simfphys_spritedamage" )
 			net.WriteEntity( self )
-			net.WriteVector( self:WorldToLocal( DamagePos ) ) 
-			net.WriteBool( false ) 
+			net.WriteVector( self:WorldToLocal( DamagePos ) )
+			net.WriteBool( false )
 		net.Broadcast()
-		
+
 		self:ApplyDamage( Damage, Type )
 	end
 end
@@ -252,7 +250,7 @@ local function Spark( pos , normal , snd )
 	effectdata:SetOrigin( pos - normal )
 	effectdata:SetNormal( -normal )
 	util.Effect( "stunstickimpact", effectdata, true, true )
-	
+
 	if snd then
 		sound.Play( Sound( snd ), pos, 75)
 	end
@@ -268,20 +266,20 @@ function ENT:PhysicsCollide( data, physobj )
 			return
 		end
 	end
-	
+
 	if ( data.Speed > 60 && data.DeltaTime > 0.2 ) then
-		
+
 		local pos = data.HitPos
-		
+
 		if (data.Speed > 1000) then
 			Spark( pos , data.HitNormal , "MetalVehicle.ImpactHard" )
-			
+
 			self:HurtPlayers( 5 )
-			
+
 			self:TakeDamage( (data.Speed / 7) * simfphys.DamageMul, Entity(0), Entity(0) )
 		else
 			Spark( pos , data.HitNormal , "MetalVehicle.ImpactSoft" )
-			
+
 			if data.Speed > 250 then
 				local hitent = data.HitEntity:IsPlayer()
 				if not hitent then
@@ -290,10 +288,10 @@ function ENT:PhysicsCollide( data, physobj )
 					end
 				end
 			end
-			
+
 			if data.Speed > 500 then
 				self:HurtPlayers( 2 )
-				
+
 				self:TakeDamage( (data.Speed / 14) * simfphys.DamageMul, Entity(0), Entity(0) )
 			end
 		end
